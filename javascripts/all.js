0e6c2cd7785526532084a3f890dd40fe4ab43252
TouNeko <touneko@gmail.com>
2015-06-11 01:20:59 +0800

    新增 lightbox 與 team 頁面的 slick

--- javascripts/all.js
+++ javascripts/all.js
@@ -18,8 +18,46 @@ $('.slick-slider').slick({
   ]
 });
 
+$('.member-list').slick({
+  infinite: false,
+  arrows: true,
+  autoplay: false,
+  slidesToShow: 1,
+  mobileFirst: true,
+  responsive: [
+    {
+      breakpoint: 560,
+      settings: { slidesToShow: 2 }
+    },{
+      breakpoint: 1024,
+      settings: { slidesToShow: 3 }
+    },{
+      breakpoint: 1420,
+      settings: { slidesToShow: 5 }
+    }
+  ]
+});
+
  if ( $('.add-more-member').length != 0 ) {
   $('.add-more-member').on("click", function(){
     $(this).remove();
   });
  }
+if ( $('.gallery-item-inner').length != 0 ) {
+  $('.gallery-item-inner').magnificPopup({
+    type:'image',
+    removalDelay: 300,
+    mainClass: 'mfp-fade',
+    gallery: {
+      enabled: true
+    }
+  });
+}
+
+if ( $('.team-video-play').length != 0 ) {
+  $('.team-video-play').magnificPopup({
+    type:'iframe',
+    removalDelay: 300,
+    mainClass: 'mfp-fade'
+  });
+}
