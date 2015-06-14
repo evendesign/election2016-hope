0c0ccf30355effac94bcb1c728a3ad5c95720d9a
shengli1989 <shengli@shenglis-MBP.local>
2015-06-14 15:24:55 +0800

    暫時關閉 footer 效果

--- javascripts/all.js
+++ javascripts/all.js
@@ -58,38 +58,6 @@ var scale_content = function(){
       $("html, body").scrollTop(doc.height());
     }
   });
-
-  // var doc = $(document);
-  // var body = $('body');
-  // var page = $('.page');
-  // var desktop_breakpoint = 960;
-  // body.addClass('scale-content');
-
-  // $(window).on('scroll', function(){
-  //   var scrolling = doc.scrollTop();
-  //   var touch_bottom = body.height() - $(window).height();
-  //   if (scrolling >= touch_bottom) {
-  //     var viewport_width = $(window).width();
-  //     if (viewport_width >= desktop_breakpoint) {
-  //       var x = -($(window).height() * 0.8) + 'px';
-  //       page.css('transform', 'translateY('+x+')');
-  //       body.addClass('is-scaled');
-  //     }
-  //   } else {
-  //     body.removeClass('is-scaled');
-  //     page.css('transform', 'translateY(0)');
-  //   }
-  // });
-
-  // $(window).resize(function() {
-  //   var viewport_width = $(window).width();
-  //   if (viewport_width < desktop_breakpoint) {
-  //     body.removeClass('scale-content');
-  //     page.css('transform', 'translateY(0)');
-  //   } else {
-  //     body.addClass('scale-content');
-  //   }
-  // });
 }
 
 //
@@ -111,7 +79,7 @@ if( is_mobile == true) {
 
 } else {
 
-  scale_content();
+  // scale_content();
 
 }
 
