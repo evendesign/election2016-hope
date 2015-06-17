fd5fd4ce441a47da5349ae6938233b475ed0105a
shengli1989 <shengli@evendesign.tw>
2015-06-17 19:53:00 +0800

    新增團隊資料庫

--- view_helpers.rb
+++ view_helpers.rb
@@ -6,6 +6,12 @@ module ViewHelpers
     return d
   end
 
+  # yaml team_data
+  def team_data
+    d||=YAML.load_file(File.join(File.dirname(__FILE__), 'team_data.yml'))
+    return d
+  end
+
   # nav li active link
   def nav_link_to(name, href, options={})
     if href == request.path.gsub(/[^\/]+$/, '')
